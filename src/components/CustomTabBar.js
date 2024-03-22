13e14bcab7433947d2e2e44edf90c7e30d453f52
Usmanq <westerngravity@gmail.com>
2024-03-22 07:57:13 +0500

    images added  + library changed for circle progress

--- src/components/CustomTabBar.js
+++ src/components/CustomTabBar.js
@@ -1,27 +1,43 @@
-import React, { useRef } from 'react';
-import {View, TouchableOpacity, StyleSheet, Text} from 'react-native';
+import React, {useRef} from 'react';
+import {
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  Image,
+  Dimensions,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import RBSheet from 'react-native-raw-bottom-sheet';
 
 const CustomTabBar = ({state, descriptors, navigation}) => {
-	console.log("inside customtab bar state" ,state)
-	console.log("inside customtab bar=, descriptor " ,descriptors)
-	console.log("inside customtab bar  navigation" ,navigation)
-	const refRBSheet= useRef(null);
+  console.log('inside customtab bar state', state);
+  console.log('inside customtab bar=, descriptor ', descriptors);
+  console.log('inside customtab bar  navigation', navigation);
+  const refRBSheet = useRef(null);
+  const screenWidth = Dimensions.get('window').width;
+  const buttonImages = [
+    // Replace these with your actual PNG image sources
+    require('../assets/apple.png'),
+    require('../assets/apple.png'),
+    require('../assets/apple.png'),
+    require('../assets/apple.png'),
+		
+  ];
 
-	const onPress = (route, index,isFocused) => {
-		// console.log("this is the route and its index" , route,index)
+  const onPress = (route, index, isFocused) => {
+    // console.log("this is the route and its index" , route,index)
 
-		// 	const event = navigation.emit({
-		// 		type: 'tabPress',
-		// 		target: route.key,
-		// 	});
+    // 	const event = navigation.emit({
+    // 		type: 'tabPress',
+    // 		target: route.key,
+    // 	});
 
-		// 	if (!isFocused && !event.defaultPrevented) {
-		// 		navigation.navigate(route.name);
-		// 	}
-		if (route.name === '+') {
-			 refRBSheet.current.open()
+    // 	if (!isFocused && !event.defaultPrevented) {
+    // 		navigation.navigate(route.name);
+    // 	}
+    if (route.name === '+') {
+      refRBSheet.current.open();
       // Open bottom sheet or perform any action you want
       // alert('Open bottom sheet or perform any action here');xs
     } else {
@@ -35,8 +51,7 @@ const CustomTabBar = ({state, descriptors, navigation}) => {
       // Navigate to the tab screen
       navigation.navigate(route.name);
     }
-		};
-
+  };
 
   return (
     <View style={styles.tabBar}>
@@ -44,15 +59,27 @@ const CustomTabBar = ({state, descriptors, navigation}) => {
         const {options} = descriptors[route.key];
         const isFocused = state.index === index;
 
-       
-				console.log("this is the route and its index" , route,index,options, isFocused)
+        console.log(
+          'this is the route and its index',
+          route,
+          index,
+          options,
+          isFocused,
+        );
 
         return (
           <TouchableOpacity
             key={index}
-            style={{...styles.tabItem, backgroundColor: route.name === '+' ? "#ff6833" : "#272a3b"}}
-            onPress={()=>{onPress(route, index,isFocused)}}>
-						<Text style={{color:isFocused? '#ff6833': 'grey'}}>{route.name}</Text>
+            style={{
+              ...styles.tabItem,
+              backgroundColor: route.name === '+' ? '#ff6833' : '#272a3b',
+            }}
+            onPress={() => {
+              onPress(route, index, isFocused);
+            }}>
+            <Text style={{color: isFocused ? '#ff6833' : 'grey'}}>
+              {route.name}
+            </Text>
             {/* <Icon
               name={options.iconName}
               size={40}
@@ -69,7 +96,7 @@ const CustomTabBar = ({state, descriptors, navigation}) => {
         }}>
         <Icon name="rocket" size={32} color="#007AFF" />
       </TouchableOpacity> */}
-			  <RBSheet
+      <RBSheet
         ref={refRBSheet}
         customStyles={{
           wrapper: {
@@ -85,11 +112,22 @@ const CustomTabBar = ({state, descriptors, navigation}) => {
         }}
         customAvoidingViewProps={{
           enabled: false,
-        }}
-      >
+        }}>
         {/* Content of the bottom sheet */}
         <View style={styles.bottomSheetContent}>
-          <Text style={{color:'black'}}>Bottom Sheet Content will be placed here</Text>
+          {buttonImages.map((imageSource, index) => (
+            <TouchableOpacity
+              key={index}
+              style={{
+                ...styles.buttonContainer,
+                width: screenWidth * 0.2,
+                height: screenWidth * 0.2,
+                borderRadius: 40,
+              }}>
+              <Image source={imageSource} style={styles.buttonImage} />
+							<Text>Some Page</Text>
+            </TouchableOpacity>
+          ))}
         </View>
       </RBSheet>
     </View>
@@ -109,7 +147,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-
   },
   addButton: {
     position: 'absolute',
@@ -123,9 +160,29 @@ const styles = StyleSheet.create({
     elevation: 4,
     zIndex: 100,
   },
-	bottomSheetContent: {
+  bottomSheetContent: {
     alignItems: 'center',
     padding: 20,
+		flexDirection:"row"
+  },
+  bottomSheetContent: {
+		flexDirection:"row",
+    alignItems: 'center', // Center buttons horizontally (optional)
+    justifyContent: 'space-around', // Distribute buttons evenly (optional)
+  },
+  buttonContainer: {
+    // borderRadius: width * 0.1, // Set borderRadius to 10% of screen width for circular shape
+    backgroundColor: '#fff', // Button background color (customize)
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.25,
+    shadowRadius: 4, // Add optional shadow for depth
+  },
+  buttonImage: {
+    width: '100%', // Fill button container with image
+    height: '100%',
   },
 });
 
--- src/components/ProgressRings.js
+++ src/components/ProgressRings.js
@@ -1,16 +1,16 @@
 import React from 'react';
 // import { G, Circle } from 'react-native-svg';
-import { ProgressCircle } from 'react-native-svg-charts';
-import { View, Text, StyleSheet } from 'react-native';
+// import { ProgressCircle } from 'react-native-svg-charts';
+import * as Progress from 'react-native-progress';
+import {View, Text, StyleSheet} from 'react-native';
 
-
-const ProgressRing = ({ circleType , amountOfCalories }) => {
+const ProgressRing = ({circleType, amountOfCalories}) => {
   const totalCalories = 250;
   const progress = amountOfCalories / totalCalories;
 
   return (
-    <View style={{ borderWidth:1, alignItems:'center'}}>
-    <ProgressCircle
+    <View style={{ flex:1 ,alignItems: 'center', justifyContent:'space-between'}}>
+      {/* <ProgressCircle
       style={{ height: 200 }}
       progress={progress}
       progressColor={'#b060c1'}
@@ -20,8 +20,17 @@ const ProgressRing = ({ circleType , amountOfCalories }) => {
       strokeWidth={10}
       
     >
-    </ProgressCircle>
-    <Text>{circleType}</Text>
+    </ProgressCircle> */}
+      <Progress.Circle
+        size={80}
+        progress={progress}
+        color={'#b060c1'}
+        unfilledColor={'#328e8e'}
+        borderWidth={0.5}
+        indeterminate={false}
+        thickness={8}
+      />
+      <Text style={{color: 'white' ,marginTop:30 ,fontWeight:"bold" ,fontSize:15}}>{circleType}</Text>
     </View>
   );
 };
